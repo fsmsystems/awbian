d5bce96542e1384531c34d962bb688bb5a6a76af
fsmsystems <fsm.systems@gmail.com>
2014-10-21 12:09:10 +0200

    the awesome awbian theme

---
+++ .config/awesome/rc.lua
@@ -0,0 +1,18 @@
+local rc, err = loadfile(os.getenv("HOME").."/.config/awesome/awesome.lua");
+if rc then
+    rc, err = pcall(rc);
+    if rc then
+        return;
+    end
+end
+
+dofile("/etc/xdg/awesome/rc.lua");
+
+for s = 1,screen.count() do
+    mypromptbox[s].text = awful.util.escape(err:match("[^\n]*"));
+end
+
+local f = io.open(os.getenv("HOME").."/.awesome.err", "w+")
+f:write("Awesome crashed during startup on ", os.date("%B %d, %H:%M:\n\n"))
+f:write(err, "\n");
+f:close();
